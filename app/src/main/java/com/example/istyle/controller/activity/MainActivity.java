df9f4ffef59344c9dff619e3ba3dbe77dfb21888
MarrySherlock <3606515250@qq.com>
2022-02-08 06:53:16 +0800

    底部导航栏搭建

--- app/src/main/java/com/example/istyle/controller/activity/MainActivity.java
+++ app/src/main/java/com/example/istyle/controller/activity/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.istyle.controller.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.istyle.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.forum:
+                    textView.setText(R.string.title_forum);
+                    return true;
+                case R.id.wardrobe:
+                    textView.setText(R.string.title_wardrobe);
+                    return true;
+                case R.id.message:
+                    textView.setText(R.string.title_message);
+                    return true;
+                case R.id.person_img:
+                    textView.setText(R.string.title_person_img);
+                    return true;
+                case R.id.person_info:
+                    textView.setText(R.string.title_person_info);
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.test_message);
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
 }
