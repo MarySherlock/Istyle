9a2e297aa2a479f0c4d7ecbe9621107636d9a777
MarrySherlock <3606515250@qq.com>
2022-02-07 15:43:07 +0800

    start view

--- app/src/main/java/com/example/istyle/controller/activity/MainActivity.java
+++ app/src/main/java/com/example/istyle/controller/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.istyle;
+package com.example.istyle.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.istyle.R;
 
 public class MainActivity extends AppCompatActivity {
 
